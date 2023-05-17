24175ada8e62205756fa052ebcb10bcd2838a235
MariaSantana10 <c.santana@escolar.ifrn.edu.br>
2023-05-17 17:20:50 -0300

    Update 1°ORGANIZAÇÃO.py
    Organizei as listas

---
+++ 1°ORGANIZAÇÃO.py
@@ -0,0 +1,21 @@
+import random
+cores = ['vermelho' ,'azul','verde','amarelo']
+verde = ['folha','grama','lagarta','gafanhoto']
+vermelho = ['morango','tomate','cereja','flor']
+amarelo = ['girassol','sol','banana','mel']
+azul = ['mirtilo','oceano','borboleta','flor']
+verde = random.choice(verde)
+vermelho = random.choice(vermelho)
+amarelo = random.choice(amarelo)
+azul = random.choice(azul)
+print (azul)
+print(amarelo)
+print(vermelho)
+print(verde)
+
+#ainda vou organizar o resto
+#num = int(input('escreva um ou zero  '))
+#if num == 0:
+ # print(vermelho)
+#if num == 1 :
+ # print(azul)
