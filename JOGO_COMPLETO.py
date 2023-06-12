bb2db95af741516ef674f0e7254d6a39fbba0c36
MariaSantana10 <c.santana@escolar.ifrn.edu.br>
2023-06-12 10:10:26 -0300

    Update JOGO_COMPLETO.py
    fiz a alteração que eu comentei em sala de aula

--- JOGO_COMPLETO.py
+++ JOGO_COMPLETO.py
@@ -25,64 +25,79 @@ tempo()
 
 from random import choice
 
-verde = ['folha','grama','lagarta','trevo']
-vermelho = ['morango','tomate','cereja','flor']
-amarelo = ['estrela','sol','banana','mel']
-azul = ['mirtilo','oceano','baleia','flor']
+verdes = ['folha','grama','lagarta','trevo']
+vermelhas = ['morango','tomate','cereja','flor']
+amarelas = ['estrela','sol','banana','mel']
+azuis = ['mirtilo','oceano','baleia','flor']
 
-verde = choice(verde).upper()
-vermelho = choice(vermelho).upper()
-amarelo = choice(amarelo).upper()
-azul = choice(azul).upper()
+verde = choice(verdes).upper()
+vermelho = choice(vermelhas).upper()
+amarelo = choice(amarelas).upper()
+azul = choice(azuis).upper()
+
+dica=verdes
+
 
 letras_ditas=[]
 
+
 estado_atual=[' _ ']
+
 
 tentativas=0
 
-chances=8
-def dica0(verdes):
-  if verdes[0]:
-    print('\n\033[1;35mDICA:\033[m','TEM EM ÁRVORES\n')
-  elif verdes[1]:
-    print('\n\033[1;35mDICA:\033[m','TEM NO CAMPO; TEM NO QUINTAL DE CASA\n')
-  elif verdes[2]:
-    print('\n\033[1;35mDICA:\033[m','VIRA BORBOLETA\n')
-  elif verdes[3]:
-    print('\n\033[1;35mDICA:\033[m','CONHECIDO POR TER 4 FOLHAS.\n')
-def dica1(vermelhas):
-  if vermelhas[0]:
-    print('\n\033[1;35mDICA:\033[m','FRUTA VERMELHA; SABOR DOS BATONS INFANTIS\n')
-  elif vermelhas[1]:
-    print('\n\033[1;35mDICA:\033[m','CONHECIDO POR SER FRUTA OU VERDURA\n')
-  elif vermelhas[2]:
-    print('\n\033[1;35mDICA:\033[m','PARECE UMA ACEROLA; DECORAÇÃO DE BOLO\n')
-  elif vermelhas[3]:
-    print('\n\033[1;35mDICA:\033[m','AS MULHERES GANHAM DOS SEUS COMPANHEIROS EM DATAS ESPECIAIS\n')
-def dica2(amarelas):
 
-  if amarelas[0]:
-    print('\n\033[1;35mDICA:\033[m','NA VIA LÁCTEA O SOL É CONHECIOD COMO UMA?\n')
-  elif amarelas[1]:
-    print('\n\033[1;35mDICA:\033[m','NA VIA LÁCTEA É CONHECIDO COMO UMA ESTRELA\n')
-  elif amarelas[2]:
-    print('\n\033[1;35mDICA:\033[m','COMIDA FAVORITA DOS MINIONS\n')
-  elif amarelas[3]:
-    print('\n\033[1;35mDICA:\033[m','VEM DAS ABELHAS\n')
-def dica3(azuis):
-  if azuis[0]:
-    print('\n\033[1;35mDICA:\033[m','NOME DE UMA PERSONAGEM DO DESENHO DA MORANGUINHO\n')
-  elif azuis[1]:
-    print('\n\033[1;35mDICA:\033[m','SÓ 5% FOI EXPLORADO\n')
-  elif azuis[2]:
-    print('\n\033[1;35mDICA:\033[m','MAIOR ANIMAL QUE EXISTE\n')
-  elif azuis[3]:
-    print('\n\033[1;35mDICA:\033[m','AS MULHERES GANHAM DOS SEUS COMPANHEIROS EM DATAS ESPECIAIS\n')
+chances=8
+
+
+
+# def dica0(verdes):
+#   if verdes[0]:
+#     print('\n\033[1;35mDICA:\033[m','TEM EM ÁRVORES\n')
+#   elif verdes[1]:
+#     print('\n\033[1;35mDICA:\033[m','TEM NO CAMPO; TEM NO QUINTAL DE CASA\n')
+#   elif verdes[2]:
+#     print('\n\033[1;35mDICA:\033[m','VIRA BORBOLETA\n')
+#   elif verdes[3]:
+#     print('\n\033[1;35mDICA:\033[m','CONHECIDO POR TER 4 FOLHAS.\n')
+# def dica1(vermelhas):
+#   if vermelhas[0]:
+#     print('\n\033[1;35mDICA:\033[m','FRUTA VERMELHA; SABOR DOS BATONS INFANTIS\n')
+#   elif vermelhas[1]:
+#     print('\n\033[1;35mDICA:\033[m','CONHECIDO POR SER FRUTA OU VERDURA\n')
+#   elif vermelhas[2]:
+#     print('\n\033[1;35mDICA:\033[m','PARECE UMA ACEROLA; DECORAÇÃO DE BOLO\n')
+#   elif vermelhas[3]:
+#     print('\n\033[1;35mDICA:\033[m','AS MULHERES GANHAM DOS SEUS COMPANHEIROS EM DATAS ESPECIAIS\n')
+# def dica2(amarelas):
+#   if amarelas[0]:
+#     print('\n\033[1;35mDICA:\033[m','NA VIA LÁCTEA O SOL É CONHECIOD COMO UMA?\n')
+#   elif amarelas[1]:
+#     print('\n\033[1;35mDICA:\033[m','NA VIA LÁCTEA É CONHECIDO COMO UMA ESTRELA\n')
+#   elif amarelas[2]:
+#     print('\n\033[1;35mDICA:\033[m','COMIDA FAVORITA DOS MINIONS\n')
+#   elif amarelas[3]:
+#     print('\n\033[1;35mDICA:\033[m','VEM DAS ABELHAS\n')
+# def dica3(azuis):
+#   if azuis[0]:
+#     print('\n\033[1;35mDICA:\033[m','NOME DE UMA PERSONAGEM DO DESENHO DA MORANGUINHO\n')
+#   elif azuis[1]:
+#     print('\n\033[1;35mDICA:\033[m','SÓ 5% FOI EXPLORADO\n')
+#   elif azuis[2]:
+#     print('\n\033[1;35mDICA:\033[m','MAIOR ANIMAL QUE EXISTE\n')
+#   elif azuis[3]:
+#     print('\n\033[1;35mDICA:\033[m','AS MULHERES GANHAM DOS SEUS COMPANHEIROS EM DATAS ESPECIAIS\n')
+  
+def dec(txt):
+  print()
+  print('--'*27)
+  print('=='*10,txt,'=='*10)
+  print('--'*27)
+  print()
 def mostrar_tamanho(lista):
   print(estado_atual*len(lista))
   print('\n\033[1;35m>>>\033[m','A palavra misteriosa tem',len(lista),'letras')
-  # print(lista)
+  print(lista)
   print('\n','--'*27)
 def letra_pertence(lista):
   letra=str(input('\n\n\033[4;35mDigite uma letra:\033[m '))
@@ -109,13 +124,13 @@ def final_de_jogo(lista):
     print('\n\033[1;35m>>>\033[m','A PALAVRA CORRETA ERA: ',(lista))
   else:
     print('\n\033[1;35m>>>\033[m','VOCÊ GANHOU!')
-dec('\033[4;35mVAMOS COMEÇAR!\033[m')
-tempo()
+
 num=int(input('\033[1;35m>>>\033[mEscolha um numero de 0 á 3: '))
 print('\n','--'*27)
+
 if num == 0:
   print('\n\033[1;35m>>>\033[m','A cor que você escolheu foi: \033[1;32mVERDE\033[m\n')
-  dica0(verdes)
+  # dica0(verdes)
   mostrar_tamanho (verde)
   estado_atual= estado_atual*len(verde)
   while tentativas<chances and ''. join(estado_atual) != verde:
@@ -132,7 +147,7 @@ if num == 0:
   final_de_jogo(verde)
 if num == 1:
   print('\n\033[1;35m>>>\033[m','A cor que você escolheu foi: \033[1;31mVERMELHO\033[m\n')
-  dicas1(vermelhas)
+  # dica1(vermelhas)
   mostrar_tamanho(vermelho)
   estado_atual= estado_atual*len(vermelho)
   while tentativas<chances and ''. join(estado_atual) != vermelho:
@@ -148,7 +163,7 @@ if num == 1:
   final_de_jogo(vermelho)
 if num == 2:
   print('\n\033[1;35m>>>\033[m','A cor que você escolheu foi: \033[1;33mAMARELO\033[m\n')
-  dicas2(amarelas)
+  # dica2(amarelas)
   mostrar_tamanho(amarelo)
   estado_atual= estado_atual*len(amarelo)
   while tentativas<chances and ''. join(estado_atual) != amarelo:
@@ -164,7 +179,7 @@ if num == 2:
   final_de_jogo(amarelo)
 if num == 3:
   print('\n\033[1;35m>>>\033[m','A cor que você escolheu foi: \033[1;36mAZUL\033[m\n')
-  dicas3(azuis)
+  # dica3(azuis)
   mostrar_tamanho(azul)
   estado_atual= estado_atual*len(azul)
   while tentativas<chances and ''. join(estado_atual) != azul:
